a112c450c6751331cb2f15ecbb49a5988f40badc
Tommy Chen <toc007@ucsd.edu>
2017-05-14 02:36:17 -0700

    Python practice

---
+++ pythonFun/nigga.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+def nigga(n):
+    for j in range(0,n):
+        msg = "Fuck"
+        for i in range(0, j):
+            msg += " nigga"
+        print msg
+
+uin = input("Fuck you want nigga?: ")
+nigga(uin)
